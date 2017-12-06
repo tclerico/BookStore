1a2e5bfb325e977dfe3c808c7ca28bbb0cdbb3d9
tclerico <timclerico@gmail.com>
2017-12-05 22:41:32 -0500

    added to the sell function in bookstore

--- BookStore.cpp
+++ BookStore.cpp
@@ -79,8 +79,30 @@ void BookStore::sell(std::string title){
                 //after the exception being caught, recall function sell(title)
         }
     } else{
+        std::cout<<"Book Not Available";
+        //get want value for new book
+        int want;
+        std::cout<<"Title will be added please enter want value: ";
+        std::cin >> want;
+        //add book to inventory with have=0
+        add(title,0,want);
 
-        std::cout<<"Not Available";
+        //get customers info for waitinglist
+        std::string name;
+        std::string phone;
+        std::string email;
+        std::string pref;
+        std::cout<<"Enter the customer's name: ";
+        std::cin >> name;
+        std::cout<<"Enter teh customer's email: ";
+        std::cin >> email;
+        std::cout<<"Enter the customer's phone number: ";
+        std::cin >> phone;
+        std::cout<<"Enter customer's prefered means of contact: ";
+        std::cin >> pref;
+
+        //add person to the waiting list for the new book
+        getBook(title)->addPerson(name,email,phone,pref);
 
     }
 
