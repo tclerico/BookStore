45176e4a01f18306bfdfa77b1ce5a48e4d0e83b6
dshane <dylanshanee@gmail.com>
2017-12-14 19:10:17 -0500

    Updated

--- BookStore.cpp
+++ BookStore.cpp
@@ -172,8 +172,8 @@ void BookStore::help(){
     std::cout<<"A  - Add a book to the inventory"<<std::endl;
     std::cout<<"M  - Modify the want value for the specified tittle"<<std::endl;
     std::cout<<"S  - Sell book"<<std::endl;
-    std::cout<<"O  - Order books based on a file"<<std::endl;
-    std::cout<<"D  - Delivery, updates books based on a file"<<std::endl;
+    std::cout<<"O  - Order books based on a current inventory"<<std::endl;
+    std::cout<<"D  - Delivery, updates books based on an order invoice"<<std::endl;
     std::cout<<"R  - Return books"<<std::endl;
     std::cout<<"Q  - Quit  \n"<<std::endl;
 }
