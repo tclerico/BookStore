70d7fa1359ceff892683c4410b70cce8a0630fd6
dshane <dylanshanee@gmail.com>
2017-12-18 20:28:12 -0500

    Testing implemented

--- main.cpp
+++ main.cpp
@@ -13,11 +13,11 @@ void runInterface(Interface* interface){
 
 
 int main() {
-
+    //Uncomment either the tester the interface to run
     //Tester* test = new Tester();
     //runTest(test);
-    Interface* interface = new Interface();
-    runInterface(interface);
+    //Interface* interface = new Interface();
+    //runInterface(interface);
 
 
     return 0;
